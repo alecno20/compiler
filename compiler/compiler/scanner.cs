e0667b6392340623a2690f824c442a99a8e5779a
alecno20 <craiga@oregonstate.edu>
2017-03-20 01:10:32 -0700

    tokenizer written

--- compiler/compiler/scanner.cs
+++ compiler/compiler/scanner.cs
@@ -2,13 +2,14 @@
 {
     class scanner
     {
-        //tokenizes file
+        //tokenizes file by calling methods
         public void scan(string fileName)
         {
             string fileContents=getFile(fileName);
             System.Collections.Generic.List<string> strings =toStrings(fileContents);
             printStrings(strings);
-
+            System.Collections.Generic.List<string[]> tokens = tokenizer(strings);
+            printTokens(tokens);
         }
         //converts file to string
         public string getFile(string fileName)
@@ -73,6 +74,40 @@
             {
                 System.Console.WriteLine(strings[i]);
             }
+        }
+        //prints tokens
+        public void printTokens(System.Collections.Generic.List<string[]> tokens)
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                System.Console.WriteLine("token- type:"+tokens[i][0]+" value:"+tokens[i][1]);
+            }
+        }
+        //converts to tokens
+        public System.Collections.Generic.List<string[]> tokenizer(System.Collections.Generic.List<string> strings)
+        {
+            System.Collections.Generic.List<string[]> tokens = new System.Collections.Generic.List<string[]>();
+            for (int i = 0; i < strings.Count; i++)
+            {   
+                //checks if it is a variable name or namespace
+                if (char.IsLetter(strings[i][0]))
+                {
+                    if(strings[i]=="void"|| strings[i] == "int")//fdsafsadfsadfsadfsafsafdsfsafffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+                    tokens.Add(new string[] { "variable", strings[i] });
+                }
+                //checks if it is a value
+                if (char.IsNumber(strings[i][0]))
+                {
+                    tokens.Add(new string[] { "value", strings[i] });
+                }
+                //check if it is an operator
+                else if (strings[i][0]==';'||strings[i][0]=='('||strings[i][0]==')'||strings[i][0]=='{'||strings[i][0]=='}'||strings[i][0]=='+'||strings[i][0]=='/'||strings[i][0]=='*'||strings[i][0]=='-'||strings[i][0]=='='||strings[i][0]=='!')
+                {
+                    tokens.Add(new string[] { "operator", strings[i] });
+                }
+
+            }
+            return tokens;
         }
 
     }
--- compiler/compiler/treeNode.cs
+++ compiler/compiler/treeNode.cs
@@ -14,11 +14,11 @@ namespace compiler
         public int nodeType; //PROGRAM, DECLARATION, etc.
         public int typeSpecifier; //VOID or INT
         public String rename; //Used by the Semantic Analyzer
-        public boolean visited; //Initialized to false, used for traversals
+        public bool visited; //Initialized to false, used for traversals
 
-        public TreeNode C1; //Pointer to Child 1
-        public TreeNode C2; //Pointer to Child 2
-        public TreeNode C3; //Pointer to Child 3
-        public TreeNode sibling; //Pointer to Sibling 
+        public treeNode C1; //Pointer to Child 1
+        public treeNode C2; //Pointer to Child 2
+        public treeNode C3; //Pointer to Child 3
+        public treeNode sibling; //Pointer to Sibling 
     }
 }
